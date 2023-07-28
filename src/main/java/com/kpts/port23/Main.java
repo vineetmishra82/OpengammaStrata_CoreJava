ebaeca92964bfed4f402a42f6d9504e1613b8e94
Vineet Mishra <vineet_mishra82@hotmail.com>
2023-07-29 03:41:48 +0530

    with adding results to a list

--- src/main/java/com/kpts/port23/Main.java
+++ src/main/java/com/kpts/port23/Main.java
@@ -121,6 +121,13 @@ public class Main {
 						
 						finalResult.put(lineNum, list);
 					}
+					
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 											
 				}
 			};
