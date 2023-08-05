6e7dc7f01c872843856c603e1a9eccb948981897
Vineet Mishra <vineet_mishra82@hotmail.com>
2023-08-05 22:56:33 +0530

    with callable

--- src/main/java/com/kpts/port23/Main.java
+++ src/main/java/com/kpts/port23/Main.java
@@ -26,11 +26,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		 long projectStartTime = System.currentTimeMillis();
-		 
-		 ExecutorService executorService = null;
-		 
-		 Map<String,List<String>> finalResult = new HashMap<>();
+		long projectStartTime = System.currentTimeMillis();
+
+		ExecutorService executorService = null;
+
+		Map<String, List<String>> finalResult = new HashMap<>();
 
 		List<Map<String, String>> itemList = new ArrayList<Map<String, String>>();
 		System.out.println("Running strata project");
@@ -53,24 +53,22 @@ public class Main {
 
 			String line = "";
 			int count = 0;
-			
+
 			int noOfRows = -1;
-			
+
 			System.out.print("\nNo of rows to read - ");
-			
+
 			try {
 				noOfRows = Integer.parseInt(scan.nextLine());
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				System.out.println("All rows will be read as u entered wrong");
 			}
-			
-			readLines:
-			while ((line = buffReader.readLine()) != null) {
-				String[] values = line.split(",");		
-				
-				if(count == noOfRows)
-				{
+
+			readLines: while ((line = buffReader.readLine()) != null) {
+				String[] values = line.split(",");
+
+				if (count == noOfRows) {
 					break readLines;
 				}
 
@@ -88,24 +86,21 @@ public class Main {
 					Map<String, String> item = new TreeMap<String, String>();
 
 					item.put("RowNo", String.valueOf(count));
-					
-					//Checking empty line
-					
+
+					// Checking empty line
+
 					int blankCount = 0;
 					for (String string : values) {
-						
-						if(string.equals(""))
-						{
+
+						if (string.equals("")) {
 							blankCount++;
 						}
 					}
-					
-					if(blankCount >= headers.size())
-					{
+
+					if (blankCount >= headers.size()) {
 						break readLines;
 					}
-					
-					
+
 					for (int i = 0; i < headers.size(); i++) {
 						item.put(headers.get(i), values[i]);
 					}
@@ -117,10 +112,8 @@ public class Main {
 
 			}
 			buffReader.close();
-			
-			System.out.println("\nTotal rows are - "+itemList.size());
-			
-			
+
+			System.out.println("\nTotal rows are - " + itemList.size());
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -132,99 +125,66 @@ public class Main {
 		} catch (NumberFormatException e) {
 			loopCount = -1;
 		}
-		
+
 		int threadCount = 8;
-		
+
 		System.out.print("\nNo of threads - ");
-		
+
 		try {
 			threadCount = Integer.parseInt(scan.nextLine());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.println("Thread count is 8 only as u entered wrong");
-		}
-		finally {
-				scan.close();
+		} finally {
+			scan.close();
 		}
 
 		int lineNo = 1;
-		 long startTime = System.currentTimeMillis();
+		long startTime = System.currentTimeMillis();
 		for (Map<String, String> item : itemList) {
-			
-						
+
 			final String lineNum = String.valueOf(lineNo);
 			List<String> resultList = new ArrayList<String>();
-			
-			finalResult.put(lineNum, resultList);
-					
 
 			double loopSize = loopCount == -1 ? Double.valueOf(item.get("Loops")) : loopCount;
-		
+
 			executorService = Executors.newFixedThreadPool(threadCount);
-						
-			List<Callable<String>> tasks = new ArrayList<>();
-			
+
 			Product product = new Product(item.get("SECURITY_SCHEME") + "," + item.get("SECURITY_VALUE"),
 					item.get("SECURITY_SCHEME") + "," + item.get("ISSUER_VALUE"),
 					Long.valueOf(item.get("QUANTITY").replace("L", "")), Double.valueOf(item.get("NOTIONAL")),
 					Double.valueOf(item.get("FIXED_RATE")), item.get("START_DATE"), item.get("END_DATE"),
 					item.get("SETTLEMENT"), Double.valueOf(item.get("CLEAN_PRICE")), item.get("VAL_DATE"),
 					String.valueOf(lineNo));
-        	
-			
-	        for (double i = 0; i < loopSize; i++) {
-	        
-	        		
-	        	tasks.add(() -> product.calculatePresentValue());
-	        }		
-	        
-	        List<Future<String>> futures;
-			try {
-				futures = executorService.invokeAll(tasks);
-				
-				for (Future<String> future : futures) {
-		            String result = future.get();
-//				    synchronized (finalResult) {
-//		                
-//		                finalResult.get(lineNum).add(result);
-//		            }
-		        }
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (ExecutionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}			
-	        
-	        
-	        // Shutdown the thread pool
-	        executorService.shutdown();
-			
-	        while(!executorService.isTerminated())
-	        {
-	        	
-	        }
-			
-			
-			System.out.println("Processed Row " + lineNo + " for " + String.valueOf(loopSize) + " times with result size "+finalResult.get(lineNum).size()+"\n");
-			
-			
 
-				lineNo++;	
+			Callable<Void> callable = () -> {
+
+				for (double i = 0; i < loopSize; i++) {
+					
+					resultList.add(product.calculatePresentValue());				
+
+				}
 				
-      
+				finalResult.put(lineNum, resultList);
+
+				return null;
+			};
 			
+			executorService.submit(callable);
+			
+			executorService.shutdown();
+	       
+			System.out.println("Processed Row " + lineNo + " for " + String.format("%.0f",loopSize)
+					+ " times with result size " + finalResult.get(lineNum).size() + "\n");
+
+			lineNo++;
+
 		}
-		
-		
-		
-		
+
 		System.out.printf("\nTime taken for calculations only : %s ms%n", System.currentTimeMillis() - startTime);
-		System.out.printf("Time taken for Entire Project with File Reading & storing results : %s ms%n", System.currentTimeMillis() - projectStartTime);
-		
+		System.out.printf("Time taken for Entire Project with File Reading & storing results : %s ms%n",
+				System.currentTimeMillis() - projectStartTime);
+
 	}
-		
-		
+
 }
-	
